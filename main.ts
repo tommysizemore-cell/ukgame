8ce0397325d60dd6a737b2a86dfaaed2a02f09fb
tommysizemore-cell <tommy.sizemore@stu.leslie.kyschools.us>
2026-06-04 15:25:41 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -14,10 +14,17 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.goal, function (sprite, otherSpr
     music.play(music.melodyPlayable(music.powerUp), music.PlaybackMode.UntilDone)
     game.gameOver(true)
 })
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    sprites.destroy(mySprite3)
+    sprites.destroy(mySprite4)
+    info.setScore(2)
+})
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
     music.play(music.melodyPlayable(music.wawawawaa), music.PlaybackMode.UntilDone)
     game.gameOver(false)
 })
+let mySprite4: Sprite = null
+let mySprite3: Sprite = null
 let mySprite: Sprite = null
 mySprite = sprites.create(img`
     . . . . . . f f f f . . . . . . 
@@ -88,5 +95,137 @@ let mySprite2 = sprites.create(img`
     `, SpriteKind.Enemy)
 mySprite2.setPosition(208, 80)
 mySprite2.vx = 50
+animation.runImageAnimation(
+mySprite,
+[img`
+    . . . . . . f f f f . . . . . . 
+    . . . . f f f 2 2 f f f . . . . 
+    . . . f f f 2 2 2 2 f f f . . . 
+    . . f f f e e e e e e f f f . . 
+    . . f f e 2 2 2 2 2 2 e e f . . 
+    . . f e 2 f f f f f f 2 e f . . 
+    . . f f f f e e e e f f f f . . 
+    . f f e f b f 4 4 f b f e f f . 
+    . f e e 4 1 f d d f 1 4 e e f . 
+    . . f e e d d d d d d e e f . . 
+    . . . f e e 4 4 4 4 e e f . . . 
+    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . f f . . f f . . . . . 
+    `,img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . f f f f . . . . . . 
+    . . . . f f f 2 2 f f f . . . . 
+    . . . f f f 2 2 2 2 f f f . . . 
+    . . f f f e e e e e e f f f . . 
+    . . f f e 2 2 2 2 2 2 e e f . . 
+    . f f e 2 f f f f f f 2 e f f . 
+    . f f f f f e e e e f f f f f . 
+    . . f e f b f 4 4 f b f e f . . 
+    . . f e 4 1 f d d f 1 4 e f . . 
+    . . . f e 4 d d d d 4 e f e . . 
+    . . f e f 2 2 2 2 e d d 4 e . . 
+    . . e 4 f 2 2 2 2 e d d e . . . 
+    . . . . f 4 4 5 5 f e e . . . . 
+    . . . . f f f f f f f . . . . . 
+    . . . . f f f . . . . . . . . . 
+    `,img`
+    . . . . . . f f f f . . . . . . 
+    . . . . f f f 2 2 f f f . . . . 
+    . . . f f f 2 2 2 2 f f f . . . 
+    . . f f f e e e e e e f f f . . 
+    . . f f e 2 2 2 2 2 2 e e f . . 
+    . . f e 2 f f f f f f 2 e f . . 
+    . . f f f f e e e e f f f f . . 
+    . f f e f b f 4 4 f b f e f f . 
+    . f e e 4 1 f d d f 1 4 e e f . 
+    . . f e e d d d d d d e e f . . 
+    . . . f e e 4 4 4 4 e e f . . . 
+    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . f f . . f f . . . . . 
+    `,img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . f f f f . . . . . . 
+    . . . . f f f 2 2 f f f . . . . 
+    . . . f f f 2 2 2 2 f f f . . . 
+    . . f f f e e e e e e f f f . . 
+    . . f e e 2 2 2 2 2 2 e f f . . 
+    . f f e 2 f f f f f f 2 e f f . 
+    . f f f f f e e e e f f f f f . 
+    . . f e f b f 4 4 f b f e f . . 
+    . . f e 4 1 f d d f 1 4 e f . . 
+    . . e f e 4 d d d d 4 e f . . . 
+    . . e 4 d d e 2 2 2 2 f e f . . 
+    . . . e d d e 2 2 2 2 f 4 e . . 
+    . . . . e e f 5 5 4 4 f . . . . 
+    . . . . . f f f f f f f . . . . 
+    . . . . . . . . . f f f . . . . 
+    `],
+100,
+true
+)
 mySprite2.setBounceOnWall(true)
 mySprite2.vx = 50
+mySprite3 = sprites.create(img`
+    ....................
+    ....................
+    ....................
+    ....................
+    ....................
+    ....................
+    .....7977777777.....
+    .....7777777777.....
+    ......66666666......
+    ......99999999......
+    .....9999999999.....
+    .....9999999999.....
+    .....9999999999.....
+    .....9999999999.....
+    .....9999999999.....
+    .....9999999999.....
+    .....9999999999.....
+    ......77777777......
+    ....................
+    ....................
+    `, SpriteKind.Food)
+mySprite3.setPosition(208, 80)
+mySprite4 = sprites.create(img`
+    ......................bbb.......
+    ....................bb333b......
+    .................bbb333d33b.....
+    ................bb333333d3a.....
+    ..............bb33332eeeedba....
+    ............bbb333323eee2e3a....
+    ..........bbd333333e22222ed3a...
+    .......bbbdd3333333e22222edda...
+    ......bb3d333333333be222eb3d3a..
+    ...bbb3dd33333333333beeeb33d3a..
+    ..b3ddd33333333333333333333dda..
+    bbddd3333333333333333333333dd3a.
+    b33dddddd3333333333333333333d3a.
+    bb3333333ddddd33333333333333dda.
+    bbbbbbb333dd33dddddddddd3333ddba
+    b55553bbbbbb3333dd33333ddd33dd3a
+    b555555555553bbbbbbbb33333dddd3a
+    bd555555555555555dddbaaaaab3d3ba
+    bb55555555555555555dddddddbb33ba
+    b3bb35555555555d5555d55dddddbbba
+    b33333bbb355dd55555d555ddddddbba
+    b5555d333333bbb35dddddd55ddddbba
+    b5d555dd5553333bbbbb3ddddddddb3a
+    b5d555555555555dd3333bbbbbb3db3a
+    bd5d555555d55555dd555ddbbbbbbb3a
+    bbb55dd555555555555555ddddddbb3a
+    ...bbbbdd555ddd5555ddddddddddb3a
+    .......bbbb555555d5ddd5ddddddb3a
+    ...........bbbb55555555555dd533a
+    ...............bbbbddd5d55d5b3ba
+    ...................bbbbddddb3ba.
+    .......................bbbaaaa..
+    `, SpriteKind.Food)
+mySprite4.setPosition(400, 48)
